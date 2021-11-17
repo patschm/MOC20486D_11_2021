57034b452375f76b6dcb1ea5ce4c20f3a2d58d30
Patrick Schmidt <pschmidt@pasit.nl>
2021-11-17 10:12:58 +0100

    dag3

---
+++ Module_6/ModellenBureau/Models/PersonViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModellenBureau.Models
+{
+    public class PersonViewModel
+    {
+        //[Display(Name = "Idee")]
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Voornaam verplicht")]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+        [DisplayName("Lastt name")]
+        //[StringLength(4, ErrorMessage = "Achternaam is te lang")]
+        [RegularExpression("[A-Za-z]+",ErrorMessage = "Invalide format" )]
+        [CapitalFirst(ErrorMessage = "Eeerste letter moet hoofdletter zijn")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+        [DataType(DataType.Password)]
+        public string DirtySecrets { get; set; }
+    }
+}
